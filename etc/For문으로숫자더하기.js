4fb3b209c46756cb3db6739889e4c3cfaad92cd2
Leejinuk <leejin7900@gmail.com>
2024-09-22 21:34:29 +0900

    Fix: For문으로 숫자더하기

--- etc/For문으로숫자더하기.js
+++ etc/For문으로숫자더하기.js
@@ -1,8 +1,15 @@
 // for문으로 1부터 10까지 더하는 코드를 짜기
 
-function addition() {
-  for (let i = 0; i < 11; i++) {
-    console.log(i);
-  }
+let sum = 0;
+for (let i = 1; i <= 10; i++) {
+  sum = sum + i;
 }
-addition();
+
+console.log(sum);
+
+let sum1 = 0;
+for (let i = 1; i <= 10; i++) {
+  sum1 += i;
+}
+
+console.log(sum1);
