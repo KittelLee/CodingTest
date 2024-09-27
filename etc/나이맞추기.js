c93e9ee428f659219a47dd91d20dd16c3a124e5b
Leejinuk <leejin7900@gmail.com>
2024-09-27 23:02:58 +0900

    Fix: 나이맞추기 코드수정

--- etc/나이맞추기.js
+++ etc/나이맞추기.js
@@ -1,2 +1,10 @@
 // 출생년도를 prompt로 입력받으면 그에 해당하는 나이를 출력
 // new Date()이용
+
+const date = new Date();
+const year = prompt("출생년도를 입력하세요");
+const year_of_age = date.getFullYear();
+const result = year_of_age + 1 - year;
+console.log(result);
+
+// 리팩토링 필요
